34852c6c240ec2f876e00205d19df7e24583eef3
Maiko Tan <maiko.tan.coding@gmail.com>
2022-12-05 22:32:29 +0800

    feat: trigger command by appel & prefix

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,30 @@ import { Context, Schema } from 'koishi'
 
 export interface Config {
   token: string
+  appellation: boolean
+  prefix?: string
 }
 
 export const Config: Schema<Config> = Schema.object({
   token: Schema.string().description('OpenAI 的 Session Token').required(),
+  appellation: Schema.boolean().description('是否使用称呼触发对话'),
+  prefix: Schema.string().description('使用特定前缀触发对话'),
 })
 
 export function apply(ctx: Context, config: Config) {
   ctx.i18n.define('zh', require('./locales/zh-CN'))
 
   const api = new ChatGPTAPI({ sessionToken: config.token, markdown: false })
+
+  ctx.middleware(async (session, next) => {
+    if (session.parsed?.appel) {
+      session.execute('chatgpt ' + session.parsed.content)
+    } else if (config.prefix && session.content.startsWith(config.prefix)) {
+      session.execute('chatgpt ' + session.content.slice(config.prefix.length))
+    } else {
+      return next()
+    }
+  })
 
   ctx.command('chatgpt')
     .action(async ({ session }, input) => {
