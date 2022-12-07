8b93d1fb397d7ad1ea70d8c9dc8734c9d7c253f6
Maiko Tan <maiko.tan.coding@gmail.com>
2022-12-07 14:22:01 +0800

    feat: isolate context by different appoach

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import ChatGPT from './api'
-import { Context, Logger, Schema, SessionError } from 'koishi'
+import { Context, Logger, Schema, Session, SessionError } from 'koishi'
 
 const logger = new Logger('chatgpt')
+
+const conversationContext = ['user', 'channel', 'both'] as const
+export type ConversationContext = typeof conversationContext[number]
 
 export interface Config extends ChatGPT.Config {
   appellation: boolean
   prefix: string[]
+  conversationContext: ConversationContext
 }
 
 export const Config: Schema<Config> = Schema.intersect([
@@ -16,6 +20,11 @@ export const Config: Schema<Config> = Schema.intersect([
       Schema.array(String),
       Schema.transform(String, (prefix) => [prefix]),
     ] as const).description('使用特定前缀触发对话。').default(['!', '！']),
+    conversationContext: Schema.union([
+      Schema.const('user').description('用户独立'),
+      Schema.const('channel').description('频道独立'),
+      Schema.const('both').description('频道内用户独立'),
+    ]).description('对话历史上下文缓存方式。').default('channel')
   }),
 ])
 
@@ -25,6 +34,17 @@ export function apply(ctx: Context, config: Config) {
   ctx.i18n.define('zh', require('./locales/zh-CN'))
 
   const api = new ChatGPT(ctx, config)
+
+  const getMapKey = (() => {
+    switch (config.conversationContext) {
+      case 'user':
+        return (session: Session) => session.uid
+      case 'channel':
+        return (session: Session) => session.cid
+      case 'both':
+        return (session: Session) => `${session.cid}:${session.uid}`
+    }
+  })()
 
   ctx.middleware(async (session, next) => {
     if (session.parsed?.appel) {
@@ -40,8 +60,10 @@ export function apply(ctx: Context, config: Config) {
   ctx.command('chatgpt <input:text>')
     .option('reset', '-r')
     .action(async ({ options, session }, input) => {
+      const mapKey = getMapKey(session)
+
       if (options?.reset) {
-        conversations.delete(session.uid)
+        conversations.delete(mapKey)
         return session.text('.reset-success')
       }
 
@@ -60,9 +82,9 @@ export function apply(ctx: Context, config: Config) {
 
       try {
         // send a message and wait for the response
-        const { conversationId, messageId } = conversations.get(session.uid) ?? {}
+        const { conversationId, messageId } = conversations.get(mapKey) ?? {}
         const response = await api.sendMessage({ message: input, conversationId, messageId })
-        conversations.set(session.uid, { conversationId: response.conversationId, messageId: response.messageId })
+        conversations.set(mapKey, { conversationId: response.conversationId, messageId: response.messageId })
         return response.message
       } catch (error) {
         logger.warn(error)
