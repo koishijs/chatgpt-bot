99a9d6dfa0ba661081328dfe05f2a80b647f4a01
Maiko Tan <maiko.tan.coding@gmail.com>
2022-12-07 14:07:13 +0800

    feat: make endpoint configurable
    per #8

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import ExpiryMap from 'expiry-map'
 import { createParser } from 'eventsource-parser'
-import { Context, Dict, Quester, Schema, SessionError } from 'koishi'
+import { Context, Dict, Quester, Schema, SessionError, trimSlash } from 'koishi'
 import internal, { Writable } from 'stream'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -17,11 +17,13 @@ export interface Conversation {
 
 class ChatGPT {
   protected http: Quester
+  protected _endpoint: string
   // stores access tokens for up to 10 seconds before needing to refresh
   protected _accessTokenCache = new ExpiryMap<string, string>(10 * 1000)
 
   constructor(ctx: Context, public config: ChatGPT.Config) {
     this.http = ctx.http.extend(config)
+    this._endpoint = trimSlash(config.endpoint)
   }
 
   async getIsAuthenticated() {
@@ -66,7 +68,7 @@ class ChatGPT {
       parent_message_id: messageId,
     }
 
-    const url = `https://chat.openai.com/backend-api/conversation`
+    const url = this._endpoint + '/backend-api/conversation'
 
     let data: internal.Readable
     try {
@@ -140,7 +142,7 @@ class ChatGPT {
     }
 
     try {
-      const res = await this.http.get('https://chat.openai.com/api/auth/session', {
+      const res = await this.http.get(this._endpoint + '/api/auth/session', {
         headers: {
           cookie: `__Secure-next-auth.session-token=${this.config.sessionToken}`,
         },
@@ -164,6 +166,7 @@ class ChatGPT {
 namespace ChatGPT {
   export interface Config {
     sessionToken: string
+    endpoint: string
     markdown?: boolean
     headers?: Dict<string>
     proxyAgent?: string
@@ -171,6 +174,7 @@ namespace ChatGPT {
 
   export const Config: Schema<Config> = Schema.object({
     sessionToken: Schema.string().description('ChatGPT 会话令牌。').required(),
+    endpoint: Schema.string().description('ChatGPT API 的地址。').default('https://chat.openai.com/'),
     headers: Schema.dict(String).description('要附加的额外请求头。').default({
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
     }),
